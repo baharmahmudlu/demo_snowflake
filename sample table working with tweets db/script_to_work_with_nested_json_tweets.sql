bf8f80ec567ab75ae44058a1ba540d78d1f70b83
Bahar Mahmudlu <bahar_mahmudlu@epam.com>
2024-10-10 22:38:56 +0200

    Update on script_to_work_with_nested_json_tweets file

--- sample table working with tweets db/script_to_work_with_nested_json_tweets.sql
+++ sample table working with tweets db/script_to_work_with_nested_json_tweets.sql
@@ -48,3 +48,18 @@ from tweet_ingest
 select value
 from tweet_ingest
 ,table(flatten(raw_status:entities:urls));
+
+
+-- Flatten and return just the hashtag text, CAST the text as VARCHAR
+select value:text::varchar as hashtag_used
+from tweet_ingest
+,lateral flatten
+(input => raw_status:entities:hashtags);
+
+-- Add the Tweet ID and User ID to the returned table so we could join the hashtag back to it's source tweet
+select raw_status:user:name::text as user_name
+,raw_status:id as tweet_id
+,value:text::varchar as hashtag_used
+from tweet_ingest
+,lateral flatten
+(input => raw_status:entities:hashtags);
